e0c9452ab36c13e09149ec40fc731d1a0f3722bd
Nick Josevski <nickjosevski@gmail.com>
2012-06-05 10:26:00 +1000

    debugging web app, and a watcher console app to capture files during compile

---
+++ Watcher/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loc = @"C:\Users\nickjosevski.ACMELBDC\AppData\Local\Temp";
+            var ext = @"*.cs";
+
+            var watcher = new FileSystemWatcher(loc, ext);
+
+            watcher.EnableRaisingEvents = true;
+
+            watcher.Created += Created;
+
+            Console.WriteLine("waiting...");
+            Console.ReadLine();
+        }
+
+        static void Created(object sender, FileSystemEventArgs e)
+        {
+            var x = 0;
+            while (x < 1000)
+            {
+                try
+                {
+                    x++;
+                    File.Copy(e.FullPath, @"C:\dev\mailzor\Watcher\bin\Debug\" + e.Name);
+                }
+                catch (Exception ex)
+                {
+                    
+                }
+            }
+
+            Console.WriteLine("found: {0}", e.FullPath);
+        }
+    }
+}
